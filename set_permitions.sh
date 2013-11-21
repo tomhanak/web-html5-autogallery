06cecda4c50dd693900e0d92340fd8b40f362c77
Tomáš Hanák <tomas.hanak@gmail.com>
2013-11-21 19:38:04 +0100

    Fixed script to not change rights for original media files

--- set_permitions.sh
+++ set_permitions.sh
@@ -3,9 +3,10 @@
 WEB_DIR=$(pwd)
 echo Applying security rules in: ${WEB_DIR}
 
-chown -R root:http .
-#find . -type d -exec chmod u=rwx,g=rx,o= {} \;
-#find . -type f -exec chmod u=rw,g=r,o= {} \;
-find . -type d -exec chmod u=rwx,g=rwx,o= {} \;
-find . -type f -exec chmod u=rw,g=rw,o= {} \;
+find . -xdev -type d -path ./site-data/galleries -prune -o -type d -exec chown root:http {} \;
+find . -xdev -type d -path ./site-data/galleries -prune -o -type d -exec chmod u=rwx,g=rwx,o=rx {} \;
+
+find . -xdev -type f -exec chown root:http {} \;
+find . -xdev -type f -exec chmod u=rw,g=rw,o=r {} \;
+
 chmod u=rwx,g=r,o= $0
